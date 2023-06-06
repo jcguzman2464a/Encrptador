600f7744fc98d042dce788576c587d7217a31397
jcguzman2464a <jcguzman2464a@gmail.com>
2023-06-05 23:53:33 -0600

    Proyecto final

--- index.js
+++ index.js
@@ -1,33 +1,71 @@
 function encriptar() {
   let texto = document.getElementById("text").value;
+  let muneco = document.getElementById("muneco");
+  let patron = /A-Z/g;
   
-  let vacio = null
-  
+
+    let encriptaTexto = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+
+
+    if (document.getElementById("text").value != "") {
+      muneco.style.display = "none";
+      document.getElementById("mensajeTexto").textContent = "";
+      document.getElementById("parrafo").textContent = "";
+      document.getElementById("mensaje").value = encriptaTexto;
+      mensaje.style.cssText = 'height 60%; display: block';
+      btnCopiar.style.display = "block";
+      document.getElementById("text").value = "";
+
+    }
+    else {
+      muneco.style.display = "block";
+      document.getElementById("mensajeTexto").textContent = "Ningún mensaje fue encontrado";
+      document.getElementById("parrafo").textContent = "Ingresa el texto de deseas encriptar o desencriptar";
+      mensaje.style.cssText = 'height 60%; display: none';
+      btnCopiar.style.display = "none";
+
+      alert("No has ingresado ningun texto");
+    }
+ 
+
+ 
+}
+
+function desencriptar() {
+  let texto = document.getElementById("text").value;
+
   let encriptaTexto = texto
-    
-    .replace(/e/gi,"enter")
-    .replace(/i/gi,"imes")
-    .replace(/a/gi,"ai")
-    .replace(/o/gi,"ober")
-    .replace(/u/gi,"ufat");
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
 
-    
-  if (texto.lenght != 0 ) {
-    document.getElementById("mensaje").value = encriptaTexto;
-    mensaje.style.height ="60%"; 
-    document.getElementById("mensajeTexto").textContent = vacio;
-    document.getElementById("parrafo").textContent = "";
+
+  if (document.getElementById("text").value.lenght != 0) {
     muneco.style.display = "none";
-   
-   
+    document.getElementById("mensajeTexto").textContent = "";
+    document.getElementById("parrafo").textContent = "";
+    document.getElementById("mensaje").value = encriptaTexto;
+    mensaje.style.cssText = 'height 60%; display: block';
+    document.getElementById("text").value = "";
   }
 
-  else {
-    muneco.style.display = "block";
-    document.getElementById("mensajeTexto").textContent = "Ningún mensaje fue encontrado";
-    document.getElementById("parrafo").textContent = "Ingresa el texto de deseas encriptar o desencriptar";
-    mensaje.style.height = "100px";
-    alert("No has ingresado ningun texto");
-  }
 
 }
+
+function copiar() {
+  mensaje.select();
+  navigator.clipboard.writeText(mensaje.value).then(() => {
+      alert("Copiado Exitosamente");
+    })
+    .catch(() => {
+      alert("Error al copiar");
+    });
+    document.getElementById("mensaje").value="";;
+  }
